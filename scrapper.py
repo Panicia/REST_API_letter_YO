bff084c6be93863e1d61e4895308df522ba74abb
Panicia <kkk2224@yandex.ru>
2022-05-04 22:05:42 +0300

    scrapper.v1

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+
 page_link = 'http://www.yomaker.ru/yoslovar.htm'
 page_response = requests.get(page_link, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
@@ -11,5 +13,16 @@ for t in texts:
 w.close()
 
 r = open('tryYO1.txt', 'r', encoding = 'utf-8')
-for str in r:
-    print(str)
+fileWords = open('tryYO2.txt', 'w', encoding = 'utf-8')
+words = []
+i = 0
+for word in r:
+    i += 1
+    if i > 45 and re.fullmatch(r'[а-я]', word[0]):
+        m = re.split(r'\[|,|\(|\)|\n|;| |-', word)
+        for w1 in m:
+            if w1.find('ё') != -1 and w1 != '-ён':
+                words.append(w1 + '\n')
+                fileWords.write(w1 + '\n')
+r.close()
+fileWords.close()
