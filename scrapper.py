ed05d07023279093f592de1cca72ee2f7e18f07b
Panicia <kkk2224@yandex.ru>
2022-05-04 19:02:54 +0300

    nbnb

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,12 @@ page_link = 'http://www.yomaker.ru/yoslovar.htm'
 page_response = requests.get(page_link, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
 texts = page_content.find_all('p')
-w = open('try1.txt', 'w')
+
+w = open('tryYO1.txt', 'w', encoding = 'utf-8')
 for t in texts:
-    w.write(t)
+    w.write(t.text + '\n')
+w.close()
+
+r = open('tryYO1.txt', 'r', encoding = 'utf-8')
+for str in r:
+    print(str)
